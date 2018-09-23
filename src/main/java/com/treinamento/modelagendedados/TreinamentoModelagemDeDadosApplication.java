f57e385908a9e3731bbbb4a134469a14eb9b8273
Ariel Lopes <ariel-edit@hotmail.com>
2018-09-23 09:58:57 -0300

    Projeto Criado

---
+++ src/main/java/com/treinamento/modelagendedados/TreinamentoModelagemDeDadosApplication.java
@@ -0,0 +1,12 @@
+package com.treinamento.modelagendedados;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreinamentoModelagemDeDadosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TreinamentoModelagemDeDadosApplication.class, args);
+	}
+}
